990468d56149663db7a5634aeb7ea20d918310b5
Tyler <tyler.visca.barca@gmail.com>
2014-01-19 01:38:04 +0000

    create chruby install file

---
+++ ruby.sh
@@ -0,0 +1,7 @@
+# RUN THIS SCRIPT AFTER RUNNING INIT.SH //// CHANGE VERSION NUMBER IN LINK AS NECESSARY
+wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.0.tar.bz2
+tar -xjvf ruby-2.1.0.tar.bz2
+cd ruby-2.1.0
+./configure --prefix=/opt/rubies/ruby-2.1.0
+make
+sudo make install
