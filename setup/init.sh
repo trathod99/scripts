2d3507a44b4674ba7cd24c0ed72fcfee2422e5b7
Tyler <tyler.visca.barca@gmail.com>
2014-03-01 02:17:02 +0000

    fix git remote link

--- setup/init.sh
+++ setup/init.sh
@@ -56,4 +56,4 @@ sudo chsh -s /bin/zsh ubuntu
 cd ~/.ssh 
 ssh-keygen -t rsa -C "tyler.visca.barca@gmail.com"
 cd ~/scripts
-git remote set-url origin git@github.com:trathod99/scripts/repo.git
+git remote set-url origin git@github.com:trathod99/scripts.git
