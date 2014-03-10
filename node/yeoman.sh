f97cf21333b950ebda55e7a9c48c1a36c585d5b4
Tyler <tyler.visca.barca@gmail.com>
2014-03-10 03:17:18 +0000

    write a new script for installing yeoman and its dependencies

---
+++ node/yeoman.sh
@@ -0,0 +1,7 @@
+echo "INSTALLING YEOMAN"
+npm install -g yo
+echo "INSTALLING ANGULAR GENERATOR"
+npm install -g generator-angular
+echo "INSTALLING GRUNT KARMA FIX"
+npm install grunt-karma --save-dev
+
