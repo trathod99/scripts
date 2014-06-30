53f7a7aa11d07af102ecb02f2d11c3c4b2a8acaf
Tyler <tyler.visca.barca@gmail.com>
2014-06-30 00:50:59 +0000

    bug fixes

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -3,4 +3,5 @@ wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/arc
 tar -xzvf ruby-install-0.4.3.tar.gz
 cd ruby-install-0.4.3/
 sudo make install
-ruby-install ruby 2.1.0
+sudo ruby-install -i /opt/rubies/ ruby 2.1.2
+echo "ATTENTION: in order to have all ruby commands work, copy the contents of 2.1.2 in /opt/rubies/2.1.2 to /opt/rubies. Also run 'chruby 2.1.2'"
